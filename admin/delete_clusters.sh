eaacc6c28c7c0f34bf03c2cb48c991ad5d2d7aa8
Sergio Romera <sergio.romera@enterprisedb.com>
2026-03-05 15:51:20 +0100

    Version 1.0

---
+++ admin/delete_clusters.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+. ./config
+
+# Delete clusters
+for (( i=1; i<=$TOTAL_USERS; i++ ))
+do
+  username="user$i"
+  sudo su - ${username} -c "kubectl delete cluster cluster-${username}"
+done
