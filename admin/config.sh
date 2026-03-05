2dd4fa4ff80b7472021a4681b16394f3ebfcc312
Sergio Romera <sergio.romera@enterprisedb.com>
2026-03-05 10:49:42 +0100

    Version 1.0

--- admin/config.sh
+++ admin/config.sh
@@ -3,5 +3,5 @@
 export TOTAL_USERS=10
 export INSTALL_ENV="EC2"         # EC2|VAGRANT
 export ENV_USER="ec2-user"       # ec2-user|vagrant
-export SOURCE_CNP_PATH=/${ENV_USER}/user0/cnpg-hands-on
+export SOURCE_CNP_PATH=/home/${ENV_USER}/user0/cnpg-hands-on
 export K3D_CLUSTER=workshop
