7ac98180e6feb7764479706ead2fc4baf61e5656
Sergio Romera <sergio.romera@enterprisedb.com>
2026-03-07 16:14:58 +0100

    Version 1.0

--- admin/install_k3d.sh
+++ admin/install_k3d.sh
@@ -34,6 +34,9 @@ kubectl label node k3d-workshop-agent-2 role=postgres-node
 # Ports
 #sg docker -c "k3d cluster edit workshop --port-add '9001:9001@loadbalancer'"
 #sg docker -c "k3d cluster edit workshop --port-add '9000:9000@loadbalancer'"
+#Minio
+sg docker -c "k3d cluster edit workshop --port-add '9090:9090@loadbalancer'"
+# create minio Load Balancer
 
 # Check open ports
 docker ps --filter "name=serverlb" --format "table {{.Names}}\t{{.Ports}}"
@@ -41,6 +44,5 @@ docker ps --filter "name=serverlb" --format "table {{.Names}}\t{{.Ports}}"
 # Share k8s config file
 sudo mkdir -p /usr/local/share/k8s
 sudo chmod 755 /usr/local/share/k8s
-#sg docker -c "k3d kubeconfig get ${K3D_CLUSTER} | sudo tee /usr/local/share/k8s/k3d-config > /dev/null"
-cp /usr/local/share/k8s/k3d-config ~/.kube/config
+sg docker -c "k3d kubeconfig get ${K3D_CLUSTER} | sudo tee /usr/local/share/k8s/k3d-config > /dev/null"
 sudo chmod 644 /usr/local/share/k8s/k3d-config
